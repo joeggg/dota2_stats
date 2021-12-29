ab116e67dc9dd32e20935944d632df3f2ab24f96
joeggg <ninjoetsu@gmail.com>
2021-12-29 01:48:23 +0000

    Do matches requests asynchronously

--- src/queries.py
+++ src/queries.py
@@ -1,10 +1,10 @@
 """
     Fetching and processing match data
 """
-from typing import Iterator
+import asyncio
+from typing import Iterator, Optional
 
-import requests
-from requests.exceptions import HTTPError
+from aiohttp import ClientError, ClientSession
 
 
 HISTORY_ENDPOINT = "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1"
@@ -14,40 +14,57 @@ with open("secret/steam_key.txt", "r") as ffile:
     KEY = ffile.read()
 
 
-def get_match_data(match_id: str) -> dict:
+async def async_request(
+    url: str,
+    params: Optional[dict] = None,
+    method: str = "GET",
+) -> dict:
+    """Make an async HTTP request safely"""
+    params = params or {}
     try:
-        resp = requests.get(
-            DETAILS_ENDPOINT,
-            params={
-                "key": KEY,
-                "match_id": match_id,
-            },
-        )
-        resp.raise_for_status()
-    except HTTPError as exc:
-        print(f"Details request failed for match {match_id}: {exc}")
-        return None
-    data = resp.json()
-    return data["result"]
-
-
-def get_matches(account_id: str, num_matches: int = 5) -> Iterator[dict]:
-    try:
-        resp = requests.get(
-            HISTORY_ENDPOINT,
-            params={
-                "key": KEY,
-                "account_id": account_id,
-                "matches_requested": num_matches,
-            },
-        )
-        resp.raise_for_status()
-    except HTTPError as exc:
+        async with ClientSession() as session:
+            async with session.request(method, url, params=params) as resp:
+                resp.raise_for_status()
+                data = await resp.json()
+                return data
+    except ClientError as exc:
         print(exc)
+        return {}
+
+
+async def get_match_data(match_id: str) -> dict:
+    data = await async_request(
+        DETAILS_ENDPOINT,
+        params={
+            "key": KEY,
+            "match_id": match_id,
+        },
+    )
+    print(f"Got match {match_id}")
+    return data.get("result")
+
+
+async def get_matches(account_id: str, num_matches: int = 5) -> Iterator[dict]:
+    data = await async_request(
+        HISTORY_ENDPOINT,
+        params={
+            "key": KEY,
+            "account_id": account_id,
+            "matches_requested": num_matches,
+        },
+    )
+    if not data:
+        print("No data returned")
+        return
+    match_list = data.get("result", {}).get("matches")
+    if not match_list:
+        print("Match list in invalid format")
         return
 
-    data = resp.json()
-    match_list = data["result"]["matches"]
+    futures = []
     for match in match_list:
         match_id = match["match_id"]
-        yield get_match_data(match_id)
+        futures.append(get_match_data(match_id))
+    results = await asyncio.gather(*futures)
+
+    return results
--- src/routes.py
+++ src/routes.py
@@ -13,8 +13,8 @@ def format_response(func):
     """Adds required headers to response"""
 
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        data = func(*args, **kwargs)
+    async def wrapper(*args, **kwargs):
+        data = await func(*args, **kwargs)
         data = {"results": data}
         resp = make_response(data)
         resp.headers["Access-Control-Allow-Origin"] = "*"
@@ -25,24 +25,19 @@ def format_response(func):
 
 
 @format_response
-def status():
+async def status():
     print("Received status command")
     return "Hello world!"
 
 
 @format_response
-def matches(account_id: str) -> str:
+async def matches(account_id: str) -> str:
     """Return formatted match data to the browser"""
     if not account_id:
         return "Invalid account ID"
 
     print(f"Fetching match data for account {account_id}")
     start = time.perf_counter()
-    matches = queries.get_matches(account_id)
-    output = []
-    for match in matches:
-        print(f'Got match {match["match_id"]}')
-        output.append(match)
+    matches = await queries.get_matches(account_id)
     print(f"Time taken: {time.perf_counter() - start}s")
-
-    return output
+    return matches
