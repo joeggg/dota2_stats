ce1effd4079f1efef061c797995c35e58c116619
joeggg <ninjoetsu@gmail.com>
2021-12-29 20:08:56 +0000

    Added formatter configs

--- app/App.js
+++ app/App.js
@@ -3,13 +3,13 @@ import logo from './dota2.svg';
 import './App.css';
 
 function App() {
-
-  const [match_data, setMatches] = useState([]);
-  const account_id = 54269634;
-  const ip = '94.11.9.194';
+  const [matchData, setMatches] = useState([]);
+  const accountId = 54269634;
+  // const ip = '94.11.9.194';
+  const ip = '127.0.0.1';
 
   useEffect(() => {
-    fetch(`http://${ip}:5656/matches/${account_id}`).then(res => {
+    fetch(`http://${ip}:5656/matches/${accountId}`).then(res => {
       res.text().then(text => {
         const matches = JSON.parse(text).results;
         const data = matches.map(match => {
@@ -19,10 +19,10 @@ function App() {
             </h5>
           );
         });
-        setMatches((<div>{data}</div>))
+        setMatches((<div>{data}</div>));
         console.log(matches);
-      })
-    })
+      });
+    });
   }, []);
 
   return (
@@ -33,7 +33,7 @@ function App() {
           Dota stuff goes here <br></br>
           Matches: 
         </p>
-        {match_data}
+        {matchData}
         <a
           className='App-link'
           href='https://dota2.com'
